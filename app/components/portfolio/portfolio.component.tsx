f047dce85c0e2d7973ece40195c557454196ccaa
Gustavarendd <111903425+Gustavarendd@users.noreply.github.com>
2023-06-04 18:26:53 +0200

    small changes

--- app/components/portfolio/portfolio.component.tsx
+++ app/components/portfolio/portfolio.component.tsx
@@ -14,8 +14,16 @@ const Portfolio = () => {
 
       <div className="mt-10 mb-10 grid p-0 gap-4 grid-cols-1 md:grid-cols-2  ">
         <PortfolioCard
+          title="House Hosting"
+          subtitle="A website that lets you host your home, for people to rent"
+          usedTech="#React #TS #JS #MongoDB #nextJS #prisma #Tailwind"
+          imageSrc="/images/portfolio-images/house-hosting.jpeg"
+          liveUrl="https://househosting.vercel.app/"
+          repoUrl="https://github.com/Gustavarendd/house-hosting"
+        />
+        <PortfolioCard
           title="Crown Clothing"
-          subtitle="This is a project where i used React"
+          subtitle="A clothing web-shop"
           usedTech="#React #TS #JS #Firebase"
           imageSrc="/images/portfolio-images/crown-app-small.png"
           liveUrl="https://jade-licorice-23a528.netlify.app/"
@@ -24,7 +32,7 @@ const Portfolio = () => {
         <PortfolioCard
           title="Share My Place"
           subtitle="An app that lets you find your current location, and share a link to show where you are."
-          usedTech="#React #TS #JS #Firebase"
+          usedTech="#HTML #CSS #JS"
           imageSrc="/images/portfolio-images/share-my-place-small.png"
           liveUrl="https://gustavarendd.github.io/find-my-place/"
           repoUrl="https://github.com/Gustavarendd/find-my-place"
@@ -32,18 +40,10 @@ const Portfolio = () => {
         <PortfolioCard
           title="To Do List"
           subtitle="A simple To Do list, where you can drag list items from active to finished."
-          usedTech="#React #TS #JS #Firebase"
+          usedTech="#HTML #CSS #JS"
           imageSrc="/images/portfolio-images/to-do-list-small.png"
           liveUrl="https://gustavarendd.github.io/to-do-list/"
           repoUrl="https://github.com/Gustavarendd/to-do-list"
-        />
-        <PortfolioCard
-          title="Portfolio Website"
-          subtitle="My Portfolio website."
-          usedTech="#React #TS #JS #Firebase"
-          imageSrc="/images/portfolio-images/portfolio-small.png"
-          liveUrl="https://gustavarend.netlify.app/"
-          repoUrl="https://github.com/Gustavarendd/portfolio"
         />
       </div>
     </section>
